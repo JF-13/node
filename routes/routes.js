56a0b53cb2305b17a201975c9822a625b25c6516
Jose Flores <joseflores89@gmail.com>
2017-11-16 10:57:40 -0600

    "router 404 error added"

--- routes/routes.js
+++ routes/routes.js
@@ -2,13 +2,12 @@ var path = require('path');
 var express = require('express');
 var router = express.Router();
 
+router.get("/", function(req, res, next) {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
 
-
-  router.get("/*", function(req, res){
-    res.sendFile(path.join(__dirname, "../index.html"));
-  });
-
-  
-
+router.use(function(req, res){
+  res.status(404).send("Page was not found...");
+})
 
 module.exports = router;
