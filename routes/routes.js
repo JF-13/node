61963bfe3d5ec0332259a74af6ba4c043b0b3014
Jose Flores <joseflores89@gmail.com>
2017-11-16 02:35:48 -0600

    initial commit

---
+++ routes/routes.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var express = require('express');
+var router = express.Router();
+
+
+
+  router.get("/*", function(req, res){
+    res.sendFile(path.join(__dirname, "../index.html"));
+  });
+
+  
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var routes = require('./routes/routes');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+app.use("/", routes);
+
+app.listen(PORT, function() {
+  console.log('were up on port: ' + PORT);
+});
